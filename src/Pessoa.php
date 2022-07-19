2e0036b143ebc30595d00718c3dc4eb5b890b8dc
lucas <lucasucb2020@gmail.com>
2022-07-19 15:55:02 -0300

    Get e Set

--- src/Pessoa.php
+++ src/Pessoa.php
@@ -6,7 +6,40 @@
     class Pessoa
     {
 
-        
+        public string $nome;
+        public int $idade;
+
+        //Método construtor -> usado automaticamente quando instaciamos um objeto
+
+        //visibilidade e encapsulamento
+
+        public function __construct(string $nome, int $idade)
+        {   
+            $this->nome = $nome;
+            $this->idade = $idade;
+        }
+
+        //além do método construtor, existe o destrutor
+
+        //Métodos acessores dão acesso aos dados
+
+        public function getNome()
+        {
+           return $this->nome;
+        }
+
+        public function setNome(string $nome)
+        {
+            $this->nome = $nome;
+        }
+
+        public function getIdade(){
+            return $this->idade;
+        }
+
+        public function setIdade(int $idade){
+            $this->idade = $idade;
+        }
 
 
 
--- src/index.php
+++ src/index.php
@@ -1,3 +1,23 @@
 <?php
 
     //Toda classe tem o nome do objeto que queremos criar
+
+    require_once 'Pessoa.php';
+
+
+    $Pessoa1 = new Pessoa('Pedro', 14);
+
+    //Setando valores de um objeto
+
+
+    echo "<pre>";
+
+    $Pessoa1->nome = 'Lucas';
+    $Pessoa1->idade = 15;
+
+    var_dump($Pessoa1);
+
+    echo "</pre>";
+
+
+    echo $Pessoa1->nome;
